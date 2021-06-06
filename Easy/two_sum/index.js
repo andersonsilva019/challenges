e6045de9081b0aa940fc374a8f46e832d26510e7
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-06-06 09:18:10 -0300

    :tada: solution

--- Easy/two_sum/index.js
+++ Easy/two_sum/index.js
@@ -1,8 +1,28 @@
+
+/**
+ * Runtime: 168 ms
+ */
+
 const twoSum = function (nums, target) {
-  for (let i = 0; i < (nums.length - 1); i++) {
-    let soma = nums[i] + nums[i + 1]
-    if (soma === target) return [i, i + 1]
+  for (let i = 0; i < (nums.length - 1); ++i) {
+    for (let j = i + 1; j < nums.length; ++j) {
+      if ((nums[i] + nums[j]) === target) return [i, j]
+    }
   }
 };
 
-console.log(twoSum([2, 7, 11, 15], 9))
+/**
+ * Runtime: 52 ms
+ */
+
+const twoSum2 = function (nums, target) {
+  const comp = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (comp[nums[i]] >= 0) {
+      return [comp[nums[i]], i]
+    }
+    comp[target - nums[i]] = i
+  }
+};
+
+console.log(twoSum([2, 5, 5, 11], 10))
